d49db2a1aaf9fee53bdb9fd0bff9efb315d39674
getuliobr <getulioregis@alunos.utfpr.edu.br>
2024-02-28 01:26:31 -0300

    start precalculating sbert embeddings

--- compareAlgorithms/sbert.py
+++ compareAlgorithms/sbert.py
@@ -1,3 +1,4 @@
+import pickle
 from sentence_transformers import SentenceTransformer, util
 
 def sbert(issuesTitles: list, currentTitle: str):
@@ -11,6 +12,20 @@ def sbert(issuesTitles: list, currentTitle: str):
     if currentTitle == similarTitle:
       continue
     similarTitleEmbedding = model.encode(similarTitle)
+    similarity = util.pytorch_cos_sim(currentTitleEmbedding, similarTitleEmbedding).numpy()[0]
+    mostSimilarIssueTitles.append((similarTitle, float(similarity[0])))
+
+  return mostSimilarIssueTitles
+
+def sbert_new(issuesTitles: list, currentTitle: str):
+  mostSimilarIssueTitles = []
+
+  currentTitleEmbedding = pickle.loads(currentTitle)
+
+  for similarTitle in issuesTitles:
+    if currentTitle == similarTitle:
+      continue
+    similarTitleEmbedding = pickle.loads(similarTitle)
     similarity = util.pytorch_cos_sim(currentTitleEmbedding, similarTitleEmbedding).numpy()[0]
     mostSimilarIssueTitles.append((similarTitle, float(similarity[0])))
 
